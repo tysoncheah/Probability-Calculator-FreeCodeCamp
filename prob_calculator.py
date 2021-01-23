4afe6a56ba48ae453e774ca001e432b0cb90b635
tysoncheah <76156505+tysoncheah@users.noreply.github.com>
2021-01-23 17:07:07 +0800

    Update prob_calculator.py

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,3 +1,7 @@
+#Hi there, if you have any feedback on the code,
+#feel free to contact me on linkedin
+#https://www.linkedin.com/in/tyson-cheah
+
 import copy
 import random
 # Consider using the modules imported above.
